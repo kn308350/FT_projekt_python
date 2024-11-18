15b77b6df443957f72896f09daec13ba572c711d
kn308350 <kn308350@student.polsl.pl>
2024-11-18 17:06:38 +0100

    2.

--- 1.py
+++ 1.py
@@ -1,1 +1,32 @@
 import math
+
+class Material:
+    def __init__(self, name, density, young_modulus, yield_strength):
+        self.name = name #nazwa materiału
+        self.density = density #gestosc w g/cm^3
+        self.young_modulus = young_modulus * 1e9 #moduł youga w Pa (z GPa)
+        self.yield_strength = yield_strength
+
+class Section:
+    def __init__(self, shape, dimensions):
+        self.shape = shape #kształt przekroju - prostokąt albo koło
+        self.dimension = dimensions #wymiary; przy prostokątnym {"width": x, "height": y}, przy okrągłym {"radius": r}
+
+    def pole_przekroju (self):
+        if self.shape == 'rectangle':
+            ppr = self.dimension['width'] * self.dimension['height']
+            return ppr
+        elif self.shape == 'circle':
+            ppc = math.pi * self.dimension['radius']**2
+            return ppc
+        else:
+            print('Nieobsługiwane pole przekroju')
+
+    def moment_bezwladnosci (self):
+        if self.shape == 'rectangle':
+            mbr = self.dimension['width'] * self.dimension['height']**3/12
+            return mbr
+        elif self.shape == 'circle':
+            mbc = math.pi * self.dimension ['radius']**4/4
+        else:
+            print('Nieobsługiwane pole przekroju')
